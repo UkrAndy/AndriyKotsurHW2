97acedb89139bff10fc8983766849e01de724b0d
UkrAndy <brila_andrij@ukr.net>
2019-04-04 18:21:39 +0300

    Err

--- hw2.js
+++ hw2.js
@@ -1,43 +1,33 @@
 //Task1 Задача 4. Дано масив імен. Сформувати новий масив, який складається з імен, які починаються на задану літеру.
 let arrName = ["Oleg","Andriy","Slava","Ivan","Anton"]
 let firstLetter = "A"
-let FirstNames = arrName.filter(x=>{
-    if(x[0]==firstLetter)
-    return x
-})
+let FirstNames = arrName.filter(x=> x[0]==firstLetter )
 document.write(FirstNames)
 //Task2 Задача 5. Дано ваги автомобілів на парковці. З’ясувати, чи усі автомобілі є легковими (легшими за 2000 кг).
 let carWeight = ["1500","2100","3300","300"]
-let LightWeight = carWeight.every(x=>{
-    if(x < 2000)
-        return x 
-})
+let LightWeight = carWeight.every(x=> x < 2000)
 document.write(LightWeight)
 
 //Task3 Задача 8. Розробити функцію, яка би для довільної кількості чисел знаходила добуток чисел.
-function numProduct(result,x,i,numbers){
-    return result *=x   
-}
+
 let numbers = [7,4,2,-1,7,3]
-let prod = numbers.reduce(numProduct,1)
+let prod = numbers.reduce( (result,x)=>result * x)
 document.write(prod)
 //Як зробити через стрілкові функції?
 
 //Task6 Задача 6. Дано масив, який зберігає кількість відвідувачів протягом тижня. З’ясувати, чи були дні, коли кількість відвідувачів була меншою за 10.
-function clientsPerWeek(x,i,arr){
-    if(x<10)
-        return x
-}
-let clients = [11,7,4,12,6,13]
-let correctClients = clients.some(clientsPerWeek)
+
+let clients = [11,-7,4,-12,6,13]
+clients.forEach((x,i,a)=>{  if(x<0) a[i]=0 } )
 document.write(correctClients) */
 //Task8 Задача 7. Розробити функцію, яка би для довільної кількості переданих символів визначала, кількість букв «а».
 function symbSearch(result,x,i,arr){
-    if (result == x) {
-        return x
-    }
+    if ( x =='a')
+        result++
+    return result
+    
 }
 let symbols = ["a","c","g","a","e","u"]
-let symb1 = symbols.reduce(symbSearch,"")
+let symb1 = symbols.reduce((res,x)=> x=='a'?result+1:result,0)
 document.write(symb1)
 //Не виходить!
